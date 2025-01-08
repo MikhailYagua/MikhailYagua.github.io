ae6cc2a2c6fc0920d118c36c24e370f3781c6cd9
MikhailYagua <mijesusyagua@gmail.com>
2025-01-08 15:16:50 -0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,55 @@ const logincontent = document.querySelector('.dropdown-content')
 const loginclose = document.querySelector('.boton-cerrar')
 
 loginmenu.addEventListener('click', function(){
-    console.log('abriendo')
-    logincontent.classList.add('dropdown-show-content')
+    console.log('abriendo');
+    logincontent.classList.add('dropdown-show-content');
 })
 
 loginclose.addEventListener('click', function(){
-    console.log('cerrando')
-    logincontent.classList.remove('dropdown-show-content')
+    console.log('cerrando');
+    logincontent.classList.remove('dropdown-show-content');
 })
+
+/* Confirmar formulario */
+const formulario = document.querySelector(".login-form");
+const campoUser = document.querySelector("#username");
+const campoPass = document.querySelector("#password");
+
+formulario.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    const usuario = document.getElementById("username").value;
+    const contraseña = document.getElementById("password").value;
+    
+    const isValidUser = checkUsuario(usuario);
+    const isValidPassword = checkPassword(contraseña);
+    
+    if (isValidUser && isValidPassword){
+    alert("Enviado con éxito!")
+    } else {
+        alert("ambos campos son obligatorios")
+    }
+    /* Enviar */
+})
+
+function checkUsuario(usuario){
+    if (usuario === ''){
+        console.log('inserte un usuario válido');
+        campoUser.style.border = "2px solid #AF0000";
+        return false;
+    }
+    else{
+        campoUser.style.border = "none"
+        return true;
+    }
+}
+
+function checkPassword(contraseña){
+    if (contraseña === ''){
+        campoPass.style.border = "2px solid #AF0000";
+    return false;
+    }else{
+        campoPass.style.border = "none"
+        return true;
+    }
+}
